f92714bf7402042420ca7b6e96b35c3b90ddaede
Gavin Hanna <gavhanna@gmail.com>
2016-10-23 16:22:03 +0100

    v1

---
+++ js/main.js
@@ -0,0 +1,24 @@
+;( function( $ ) {
+
+
+  // $('.nav-item a').click(function(e) {
+  //   var goTo = $(this).attr('href');
+  //   $(".works-content").animate({scrollTop:$(goTo).offset().top}, 1000);
+  //   e.preventDefault();
+  // });
+
+	$( '.swipebox' ).swipebox( {
+		useCSS : true,
+		useSVG : true,
+		initialIndexOnArray : 0,
+		hideCloseButtonOnMobile : false,
+		removeBarsOnMobile : true,
+		hideBarsDelay : 3000,
+		videoMaxWidth : 1140,
+		beforeOpen: function() {},
+		afterOpen: null,
+		afterClose: function() {},
+		loopAtEnd: false
+	} );
+
+} )( jQuery );
